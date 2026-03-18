19ea59d7aa9112e26bf853e698cff25b31dca663
MrJackStrider67 <jessalynchristiei@gmail.com>
2026-03-18 15:50:55 +0800

    :)

--- models/Movie.js
+++ models/Movie.js
@@ -1,7 +1,17 @@
-// movie db 
-// can include...
-    // id
-    // title
-    // genre
-    // year
-    // description
+//to store the movie title, the genre, the status, which user's watchlist the movie belongs to
+
+const mongoose = require("mongoose")
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    genre: String,
+    releaseYear: Number,
+    status: String,
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    createdAt: { type: Date, default: Date.now }
+})
+
+module.exports = mongoose.model("Movie", movieSchema)
--- models/User.js
+++ models/User.js
@@ -1,8 +1,15 @@
-// user db
-// can include...
-    // id
-    // name
-    // dob
-    // email
-    // username
-    // password
+// Without this file mongoDB wouldn't know the structure of the user data
+
+// Loads mongoose (the tool that connects node.js to mongodb)
+const mongoose = require("mongoose")
+
+//creates sthe schema
+const userSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    password: String,
+    role: { type: String, default: "user" }, // tells the system what permissions the user has if no specification the default is role: "user"
+    createdAt: { type: Date, default: Date.now } // automatically records when the account was created
+})
+
+module.exports = mongoose.model("User", userSchema)
