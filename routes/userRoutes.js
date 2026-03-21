eaa123330290f4b09f4cc374ea3c703130c2502b
jk-jx <kohjiaxuan.jerica@gmail.com>
2026-03-21 19:43:42 +0800

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const isLoggedIn = require("../middleware/auth");
+const { isLoggedIn } = require("../middleware/auth");
 const userController = require("../controllers/user-controllers")
 
 //reg page
