2eed5ec1c9267393324a28bf3783a3c6654b31c4
Janice <janice0975751227@gmail.com>
2026-03-21 16:30:47 +0800

    review schema

--- models/Review.js
+++ models/Review.js
@@ -6,3 +6,34 @@
     // rating
     // comment
     // created_at
+
+const mongoose = require("mongoose")
+
+// Create a new 'review' schema
+const reviewSchema = new mongoose.Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    movie:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"Movie",
+        required: true
+    },
+    comment:{
+        type: String,
+        required: true
+    },
+    rating:{
+        type: Number,
+        min: 1,
+        max: 5
+    }
+},
+    {
+        timestamps:true
+    }
+)
+
+module.exports = mongoose.model("Review", reviewSchema, "reviews")
