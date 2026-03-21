65308b7ad04c43db71e98fde1b4bacaec05585a1
Janice <janice0975751227@gmail.com>
2026-03-22 02:56:44 +0800

    move review stuff to controller, added user profile

--- server.js
+++ server.js
@@ -17,39 +17,13 @@ app.use(session({
     saveUninitialized: false
 }));
 
-//route to handle form submission (new review post)
-app.post("/myReviews", async (req, res) => {
-    const { comment, rating, movie } = req.body;
-
-    if (!comment) return res.send("Comment is required!");
-
-    try {
-        const newReview = new Review({ comment, rating, movie, user:req.session.userId, username: req.session.username});
-        await newReview.save(); // save to mongoDB
-        res.redirect('/movie');
-    } catch (err) {
-        console.error("Error saving review:", err);
-        res.send(err.message)
-    }
+//store username all pages
+app.use((req, res, next) => {
+    res.locals.username = req.session.username;
+    next();
 });
 
-//route to get the reviews page
-app.get("/myReviews", async (req,res) => {
-    try {
-        // get all reviews
-        //user and movie come from movie review schema
-        //populate = convert ID → full object from another collection
-        const reviews = await Review.find({ user: req.session.userId }).populate("user").populate("movie"); 
-        res.render("myReviews", { reviews });
-    } catch (err) {
-        console.error(err);
-        res.send("Error loading reviews");
-    }
-});
-
-//route the get all reviews for one Movie
-
-
+//routes 
 const userRoutes = require("./routes/userRoutes");
 app.use("/", userRoutes);
 
@@ -58,6 +32,9 @@ app.use("/", movieRoutes);
 
 const watchlistRoutes = require("./routes/watchlistRoutes");
 app.use("/", watchlistRoutes);
+
+const reviewRoutes = require("./routes/reviewRoute");
+app.use("/", reviewRoutes)
 
 async function connectDB() {
     try {
