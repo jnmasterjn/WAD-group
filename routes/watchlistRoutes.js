6fa659048c18664bb97d2dc48d415e6df9c84c75
jk-jx <kohjiaxuan.jerica@gmail.com>
2026-03-21 19:08:54 +0800

    Update watchlistRoutes.js

--- routes/watchlistRoutes.js
+++ routes/watchlistRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const isLoggedIn = require("../middleware/auth");
+const { isLoggedIn } = require("../middleware/auth");
 const watchlistController = require("../controllers/watchlist-controllers");
 
 // Add to watchlist
