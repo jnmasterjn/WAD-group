d0b8fee43438eb64c1774abec2db59c51feafd5e
MrJackStrider67 <jessalynchristiei@gmail.com>
2026-03-21 16:35:19 +0800

    added controllers folder and files yay

--- routes/watchlistRoutes.js
+++ routes/watchlistRoutes.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const isLoggedIn = require("../middleware/auth");
+const watchlistController = require("../controllers/watchlist-controllers");
 
 // Add to watchlist
-router.post("/watchlist/add/:id", isLoggedIn, async (req, res) => {
-    const movieId = req.params.id;
-
-    await User.findByIdAndUpdate(req.session.userId, {
-    $addToSet: { watchlist: movieId }
-    });
-
-    res.redirect("/watchlist");
-});
-
+router.post("/watchlist/add/:id", isLoggedIn, watchlistController.addMovietoWatchlist);
 // View watchlist
 router.get("/watchlist", isLoggedIn, watchlistController.viewWatchlist);
 
@@ -20,14 +12,3 @@ router.get("/watchlist", isLoggedIn, watchlistController.viewWatchlist);
 router.post("/watchlist/remove/:id", isLoggedIn, watchlistController.removeMovie);
 
 module.exports = router;
-
-//Remove
-router.post("/watchlist/remove/:id", isLoggedIn, async (req, res) => {
-    const movieId = req.params.id;
-
-    await User.findByIdAndUpdate(req.session.userId, {
-        $pull: { watchlist: movieId }
-    });
-
-    res.redirect("/watchlist");
-});
