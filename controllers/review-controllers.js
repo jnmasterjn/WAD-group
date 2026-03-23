aed1e2299babf625833eb267744c5888eed2fc27
Janice <janice0975751227@gmail.com>
2026-03-23 19:47:31 +0800

    feat: add update comment and delete comment

--- controllers/review-controllers.js
+++ controllers/review-controllers.js
@@ -11,15 +11,14 @@ exports.postReview = async(req, res) => {
     try {
         const newReview = new Review({ comment, rating, movie, user:req.session.userId, username: req.session.username});
         await newReview.save(); // save to mongoDB
-        res.redirect('/movie');
+        res.redirect(`/movie/${movie}`); //back to moview detail page
     } catch (err) {
         console.error("Error saving review:", err);
         res.send(err.message)
     }
 }
 
-
-//route to get the reviews page
+//route to get the my review page (all comments current user left)
 exports.viewMyReviews = async (req, res) => {
     try {
         // get all reviews
@@ -30,5 +29,41 @@ exports.viewMyReviews = async (req, res) => {
     } catch (err) {
         console.error(err);
         res.send("Error loading reviews");
+    }
+}
+
+// view edit review
+exports.viewEditReview = async(req, res) => {
+    const review = await Review.findById(req.params.id)
+
+    res.render("editReview", {review})
+}
+
+//edit review
+exports.editReview = async(req, res) => {
+    const {comment, rating} = req.body
+
+    try{
+        //1. find review with this ID, 
+        //2. replace comment + rating
+        await Review.findByIdAndUpdate(req.params.id, {
+            comment, 
+            rating
+        })
+        res.redirect("/myReviews")
+    }catch(err){
+        res.send("Error updating review")
+    }
+}
+
+//delete review
+exports.deleteReview = async(req, res) => {
+    try{
+        await Review.findByIdAndDelete(req.params.id)
+        res.redirect("/myReviews")
+
+    }catch(err){
+        console.log(err)
+        res.send("Error deleting review")
     }
 }
--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -11,4 +11,13 @@ router.post("/myReviews", isLoggedIn, reviewController.postReview)
 //route to get the reviews page
 router.get("/myReviews", isLoggedIn, reviewController.viewMyReviews)
 
+//edit review (show the current review page)
+router.get("/review/edit/:id", isLoggedIn,reviewController.viewEditReview)
+
+//edit review
+router.post("/review/edit/:id", isLoggedIn,reviewController.editReview)
+
+//delete review
+router.post("/review/delete/:id", isLoggedIn, reviewController.deleteReview)
+
 module.exports = router;
