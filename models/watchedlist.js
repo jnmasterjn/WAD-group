07736b775d835343d5c0adbe70a0a99014a39b49
MrJackStrider67 <jessalynchristiei@gmail.com>
2026-04-01 12:02:23 +0800

    small update for watch and watchedlist

--- models/watchedlist.js
+++ models/watchedlist.js
@@ -14,10 +14,6 @@ const watchedMoviesSchema = new mongoose.Schema({
         type: String,
         default: "",
         maxlength: [3000, "watched movies description cannot exceed 3000 characters"],
-    },
-    isArchived: {
-        type: Boolean,
-        default: false
     }
 }, {
     timestamps: true
