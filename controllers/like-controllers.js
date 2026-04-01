c60c4163d2b77226c75486025661c606953c8c14
MrJackStrider67 <jessalynchristiei@gmail.com>
2026-04-01 22:40:30 +0800

    added comments to like controller

--- controllers/like-controllers.js
+++ controllers/like-controllers.js
@@ -1,11 +1,13 @@
 const Like = require("../models/like");
 
+// Controller for the like function
 exports.toggleLike = async(req, res) => {
     try{
+        // retrieve the user id from the session and the review id from the url
         const userId = req.session.userId
         const reviewId = req.params.id;
 
-        //checking if user alr liked
+        // checking if user already liked
         const existingLike = await Like.findOne({
             user:userId,
             review:reviewId
@@ -15,12 +17,14 @@ exports.toggleLike = async(req, res) => {
             //delete ONE document from Like collection
             await Like.deleteOne({ _id: existingLike._id });
         }else {
+            // otherwise create a new like document in the Like collection
             await Like.create({
                 user:userId,
                 review:reviewId
             })
         }
 
+        // redirect back to the previous page
         const back = req.get("referer") || "/movie";
         res.redirect(back)
 
