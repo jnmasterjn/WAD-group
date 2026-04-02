eae96d3079c9d26c78527988657a82e11f129c21
qhpham2025 <qh.pham.2025@computing.smu.edu.sg>
2026-04-02 15:19:08 +0800

    indentation and comments on authentication

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,8 @@
 // middleware to protect routes
 
+// Check if user is logged in
 function isLoggedIn(req, res, next) {
+    // If no userId in session → user not logged in
     if (!req.session.userId){
         console.log("user is not logged in")
         return res.redirect("/login")
@@ -9,11 +11,15 @@ function isLoggedIn(req, res, next) {
             //move to the controller func
 }
 
+// Check if user is admin
 function isAdmin(req, res, next) {
     if (!req.session.isAdmin === true) {
         return res.status(403).send("Admins only.");
     }
+
+    // User is admin → continue
     next();
 }
 
+// Export middleware
 module.exports = { isLoggedIn, isAdmin };
