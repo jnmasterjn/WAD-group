f81f676f604c8b36715ebb35049802a353e69ddc
qhpham2025 <qh.pham.2025@computing.smu.edu.sg>
2026-04-02 17:40:33 +0800

    indentation and comment for movieroute

--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -1,9 +1,12 @@
+// ===================== IMPORTS =====================
 const express = require("express");
 const router = express.Router();
 const fs = require("fs");
 const { isLoggedIn, isAdmin } = require("../middleware/auth");
 const movieController = require("../controllers/movie-controllers")
 
+
+// Get all movies
 router.get("/movie", movieController.displayMovies);
 
 // show add movie form (needs to be above because :id is a catch-all parameter, it will match anything after /movie/ and store it in req.params.id. So instead of opening your add-movie page, it may try to find a movie whose id is "add".) 
@@ -16,14 +19,19 @@ router.get("/movie/add", isLoggedIn, isAdmin, (req, res) => {
     });
 });
 
+// Handle add movie request (admin only)
 router.post("/movie/add", isLoggedIn, isAdmin, movieController.movieAdd);
 
+// Show edit movie form (admin only)
 router.post("/movie/delete/:id", isLoggedIn, isAdmin, movieController.movieRemove);
 
+// Show edit movie form (admin only)
 router.get("/movie/edit/:id", isLoggedIn, isAdmin, movieController.movieEdit)
 
+// Show edit movie form (admin only)
 router.post("/movie/edit/:id", isLoggedIn, isAdmin, movieController.movieUpdate);
 
+// Get movie details by id  
 router.get("/movie/:id", isLoggedIn, movieController.movieDesc);
 
 module.exports = router;
